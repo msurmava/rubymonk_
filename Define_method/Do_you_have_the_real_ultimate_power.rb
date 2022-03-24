5aac6ef89ea4e6a06951040ee7c1ea82902ca55e
= <=>
2022-03-24 11:49:10 +0400

    defining_method

---
+++ Define_method/Do_you_have_the_real_ultimate_power.rb
@@ -0,0 +1,13 @@
+class Monk
+    define_method("meditate_on_life") do 
+            "I know the meaning of life"
+      end
+      define_method("meditate_on_the_universe") do 
+            "I know the meaning of the universe"
+      end
+      define_method("meditate_on_everything") do 
+            "I know the meaning of everything"
+      end
+    end
+    
+    
